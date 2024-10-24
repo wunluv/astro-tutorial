55f09a2b49a4951f021400c8937482d9a56aecb0
wunluv <santhan@mojahmedia.net>
2024-10-24 16:30:23 +0200

    deploy to production server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://thunderous-sopapillas-81fa75.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact(), tailwind()],
+  image: {
+    domains: ['localhost'],
+  },
 });
